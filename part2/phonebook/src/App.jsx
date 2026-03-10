47dcccd360a24e49188e52d8ca81399813a1dd2b
Souvik Majee <souvikmajee039@gmail.com>
2026-03-11 01:27:38 +0530

    Exercise 2.11: fetch phonebook data from server with axios

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Filter = ({ filter, handleSearchChange }) => {
   return (
@@ -34,8 +35,8 @@ const PersonForm = ({
 const Persons = ({ filteredPersons }) => {
   return (
     <ul>
-      {filteredPersons.map((person, i) => (
-        <li key={i}>
+      {filteredPersons.map((person) => (
+        <li key={person.id}>
           {person.name} &nbsp; {person.number}
         </li>
       ))}
@@ -44,15 +45,24 @@ const Persons = ({ filteredPersons }) => {
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNo, setNewNo] = useState("");
   const [filter, setFilter] = useState("");
+
+
+  useEffect(()=>{
+    const fetchPersons=async()=>{
+      const response=await axios.get("http://localhost:3001/persons");
+      setPersons(response.data);
+    }
+    fetchPersons();
+  },[])
+
+
+
+
+
 
   const handleSubmit = (event) => {
     event.preventDefault();
