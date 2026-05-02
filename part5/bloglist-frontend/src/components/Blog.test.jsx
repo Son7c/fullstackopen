baeaea16446eba897813e6acfb000ada77c31eea
Souvik Majee <souvikmajee039@gmail.com>
2026-05-02 15:55:42 +0530

    5.14

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,5 +1,6 @@
 import { render,screen } from "@testing-library/react";
 import Blog from "./Blog"
+import userEvent from '@testing-library/user-event'
 
 test('renders Blog',()=>{
     const blog={
@@ -17,4 +18,30 @@ test('renders Blog',()=>{
 
     expect(title,author).toBeDefined()
     expect(url,likes).toBeNull()
+})
+
+test('Rendering when button is clicked',async ()=>{
+    const blog={
+        title:'Testing rendering of component',
+        author:"Panda",
+        url:"https://www.google.com",
+        likes:7,
+        user:{
+            name:"Souvik Majee"
+        }
+    }
+
+    render(<Blog blog={blog}/>)
+
+    const user = userEvent.setup()
+    const btn=screen.getByText('view');
+    await user.click(btn);
+
+    const title =screen.queryByText('Testing rendering of component')
+    const author=screen.queryByText('Panda')
+    const url=screen.queryByText('https://www.google.com')
+    const likes=screen.queryByText(7);
+
+    expect(title,author,url,likes).toBeDefined()
+    screen.debug()
 })
