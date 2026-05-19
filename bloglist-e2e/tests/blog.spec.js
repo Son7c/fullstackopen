62c3f99f3c3e6d5d9b3352adb6a75a08a97b0d8d
Souvik Majee <souvikmajee039@gmail.com>
2026-05-20 00:23:49 +0530

    5.19

--- bloglist-e2e/tests/blog.spec.js
+++ bloglist-e2e/tests/blog.spec.js
@@ -21,21 +21,50 @@ describe('Blog app', () => {
     test('succeeds with correct credentials', async ({ page }) => {
       await page.getByRole('button',{name:'Login'}).click()
       await expect(page.getByText('Username')).toBeVisible()
-      const inputs= await page.getByRole('textbox').all();
-      await inputs[0].fill('son7c_142')
-      await inputs[1].fill('nnn')
+
+      await page.getByLabel('Username').fill('son7c_142');
+      await page.getByLabel('Password').fill('nnn');
+
       await page.getByRole('button',{name:"Submit"}).click()
-      await expect(page.getByText('Demo1 logged in successfully!')).toBeVisible()
+      await expect(page.getByText('Demo1 Logged in', { exact: true })).toBeVisible()
     })
 
     test('fails with wrong credentials', async ({ page }) => {
       await page.getByRole('button',{name:'Login'}).click()
       await expect(page.getByText('Username')).toBeVisible()
-      const inputs= await page.getByRole('textbox').all();
-      await inputs[0].fill('son7c_142')
-      await inputs[1].fill('nnn2')
+
+      await page.getByLabel('Username').fill('son7c_142');
+      await page.getByLabel('Password').fill('WRONG_PASSWORD');
+
       await page.getByRole('button',{name:"Submit"}).click()
       await expect(page.getByText('Wrong Credentials')).toBeVisible()
+    })
+  })
+  describe('When logged in', () => {
+    beforeEach(async ({ page ,request}) => {
+      await request.post('http://localhost:3003/api/testing/reset')
+      await request.post('http://localhost:3003/api/users',{
+        data:{
+          name:"Demo1",
+          username:"son7c_142",
+          password:"nnn"
+        }
+      })
+    })
+
+    test('a new blog can be created', async ({ page }) => {
+      await page.goto('http://localhost:5173')
+      await page.getByRole('button',{name:'Login'}).click()
+      await page.getByLabel('Username').fill('son7c_142');
+      await page.getByLabel('Password').fill('nnn');
+      await page.getByRole('button',{name:"Submit"}).click()
+
+      await page.getByRole('button',{name:'Create'}).click()
+      await page.getByLabel('title:').fill("playwright")
+      await page.getByLabel('author:').fill('son7c')
+      await page.getByLabel('url:').fill("www.google.com")
+      await page.getByRole('button',{name:'Create'}).click()
+      await expect(page.getByText('playwright by son7c')).toBeVisible()
     })
   })
 })
